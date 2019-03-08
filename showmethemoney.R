0cfccb0e55578cb051a6f45484532d40ff53ee7d
Troy Do <hoangdov@gmail.com>
2019-03-08 13:51:36 -0600

    All City

--- showmethemoney.R
+++ showmethemoney.R
@@ -8,6 +8,8 @@ library(jsonlite)
 library(mongolite)
 
 .GlobalEnv$to_export <- data.frame()
+.GlobalEnv$fort_worth <- read.csv("fort_worth.csv")
+
 
 completeFun <- function(data, desiredCols) {
   completeVec <- complete.cases(data[, desiredCols])
@@ -17,6 +19,11 @@ completeFun <- function(data, desiredCols) {
 insertMongo <- function(data) {
   con <- mongo("leads", url = "mongodb+srv://troydo42:LessIsMore42!@yeaornay-q5vdv.mongodb.net/leads?retryWrites=true")
   con$insert(data)
+}
+
+queryMongo <- function(data){
+  con <- mongo("leads", url = "mongodb+srv://troydo42:LessIsMore42!@yeaornay-q5vdv.mongodb.net/leads?retryWrites=true")
+  con$find('{"device": "mobile"}')
 }
 
 lead_gen <- function (lat, long){
@@ -57,26 +64,83 @@ lead_gen <- function (lat, long){
 
 }
 
-
 ## FORT WORTH CENTRAL : 32.758860, -97.328304
 ## NORTH POINT: 32.852772, -97.328367
 ## SOUTH POINT: 32.659375, -97.326164
 ## EAST POINT: 32.750996, -97.238101
 ## WEST POINT: 32.756011, -97.495700
+
 ## 1 degree in lattitude == 69 miles
 ## 1 mile = 1/69 = 0.014 degree
-## Coordinate File: fort_worth.csv
+## Coordinate File: fort_worth.csv  
+
+## TARANT COUNTY
+## Top Right -- 32.984215, -97.032536
+## Top Left -- 32.991833, -97.542409
+## Bottom Left -- 32.558140, -97.549819
+## Bottom Right -- 32.551466, -97.041947
 
 ## r <- plumb("showmethemoney.R")      // Write R File
 ## r$run(port=8000)                    // Launch Plumbing Server
-
 
 #* Return list of 50 businesses around a coordinate
 #* @param long Longtitude of the Business
 #* @param lat Lattitude of the Business
 #* @json
-#* @get /leads
-function(lat, long){
+#* @get /block
+oneblock <- function(lat, long){
   lead_gen(lat, long)
+}
+
+#* Query the entire city
+#* @json
+#* @get /city
+onecity <- function (){
+  for (row in 1:nrow(fort_worth)){
+    oneblock(fort_worth[row, "Var2"], fort_worth[row, "Var1"])  
+    print("Sleeping")
+    Sys.sleep(30)
+  }
+  response = "Done with Fort Worth"
+  return(response)
+}
+
+
+#* Query entire county
+#* json
+#* @get /county
+onecounty <- function(){
+  
+}
+
+#* Query entire state
+#* json
+#* @get /state
+onestate <- function(){
+  
+}
+
+
+
+#* Query entire country
+#* json
+#* @get /country
+onecountry <- function(){
+  
+} 
+
+#* Query entire region
+#* json
+#* @get /region
+oneregion <- function(){
+  
+}
+
+#* Get all mobile number
+#* @json
+#* @get /yarnumber
+function(){
+  digits <- queryMongo()
+  return(digits)
 }
 
